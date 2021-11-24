f09209ac7c12c04e286c28931b11c1493a298a47
wanger26 <jwanger@uwo.ca>
2021-11-23 20:51:04 -0500

    Hacky fix for health end point

--- src/main/java/com/stockx/subscriptionmanagerws/controllers/SubscriptionManagerController.java
+++ src/main/java/com/stockx/subscriptionmanagerws/controllers/SubscriptionManagerController.java
@@ -2,6 +2,7 @@ package com.stockx.subscriptionmanagerws.controllers;
 
 import com.stockx.subscriptionmanagerws.services.SubscriptionManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ public class SubscriptionManagerController {
 
     @Autowired
     SubscriptionManagerService subscriptionManagerService;
+
+    @GetMapping("/health")
+    public String health() {
+        return "UP";
+    }
 
     @PutMapping("/subscribe")
     public void subscribeToTopic(@RequestParam("symbol") String symbol, @RequestParam("service") String service) {
