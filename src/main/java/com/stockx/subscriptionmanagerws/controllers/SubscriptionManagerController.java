b5b719d95e7a5bac232db208766601a58d35afb6
wanger26 <jwanger@uwo.ca>
2021-12-02 23:02:44 -0500

    Fixed CORS

--- src/main/java/com/stockx/subscriptionmanagerws/controllers/SubscriptionManagerController.java
+++ src/main/java/com/stockx/subscriptionmanagerws/controllers/SubscriptionManagerController.java
@@ -2,10 +2,7 @@ package com.stockx.subscriptionmanagerws.controllers;
 
 import com.stockx.subscriptionmanagerws.services.SubscriptionManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SubscriptionManagerController {
@@ -18,11 +15,13 @@ public class SubscriptionManagerController {
         return "UP";
     }
 
+    @CrossOrigin(origins = "*")
     @PutMapping("/subscribe")
     public void subscribeToTopic(@RequestParam("symbol") String symbol, @RequestParam("service") String service) {
         subscriptionManagerService.subscribeToTopic(symbol, service);
     }
 
+    @CrossOrigin(origins = "*")
     @PutMapping("/unsubscribe")
     public void unsubscribeFromTopic(@RequestParam("symbol") String symbol) {
         subscriptionManagerService.unsubscribeFromTopic(symbol);
